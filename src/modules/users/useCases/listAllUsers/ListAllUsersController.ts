74a696ade066b3487e9f2438e9627b008d90b1ec
Michel Fonseca <michelbeserradafonseca@gmail.com>
2023-01-25 17:05:07 -0300

    Challenge done!

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,18 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    const user_id_parsed = user_id.toString();
+    try {
+      const user = this.listAllUsersUseCase.execute({
+        user_id: user_id_parsed,
+      });
+
+      return response.status(200).json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userExists = this.usersRepository.findById(user_id);
+
+    if (!userExists) {
+      throw new Error("User not found");
+    }
+    if (!userExists.admin) {
+      throw new Error("User is not admin user");
+    }
+    return this.usersRepository.list();
   }
 }
 
